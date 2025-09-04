eeefdda2576e739bed176f2255c5ac0e3ad3f1c3
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-09-04 19:12:27 +0530

    Add files via upload

---
+++ divanjali.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,peri,len=34,bd=56;
+    a=len*bd;
+    peri=2*(len+bd);
+    printf("area= %d",a);
+    printf(" perimeter= %d",peri);
+    return 0;
+}
---
+++ temperature.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int c=45;
+    float t=0.0;
+    t=(c*9/5)+32;
+    printf("temperature= %f",t);
+} 
